737edad57037933951c174655467da426a59a8c8
Luiz (Harry) Guilherme de Paula Santos <luiz.paula@embrear.com.br>
2017-07-24 10:58:35 -0300

    Making zen more modular and easier to use. Plugin and config sources are now called from a function and the interpretation of commands is a separated part fom the interactive shell.

--- zen.sh
+++ zen.sh
@@ -6,73 +6,89 @@
 		echo "" 
 	}
 
+	function zen_source() {
+		#{
+			source ~/zen/zen.sh
+
+			for f in ~/zen/plugins/*; do
+				source $f
+				echo $f
+			done
+
+			for f in ~/zen/config/*; do
+				source $f
+			done
+		#} 2>/dev/null
+	}
+
 # core
 	function zen()
 	{
-		# clear screen
-			printf "\033c"
+		case $1 in
+			# interactive shell aka zem mode
+			"")
+				# clear screen
+					printf "\033c"
 
-		# load all plugins
-			#{
-				for f in ~/zen/plugins/*
-				do
-					source $f
-				done
+				# load all plugins and configuration
+					zen_source
 
-				for f in ~/zen/config/*
-				do
-					source $f
-				done
-			#} &>/dev/null
+				# enable history
+					shopt -s histappend
 
-		# enable history
-			shopt -s histappend
+				# start event
+					zen_events start
 
-		# start event
-			zen_events start
+				# flags for communication between plugins and the core
+					zen_alive=true
+					zen_restart=false
 
-		# flags for communication between plugins and the core
-			zen_alive=true
-			zen_restart=false
+				# interactive shell
+					while ($zen_alive && ! $zen_restart)
+					do
+						read -e -p "Zen> " action context arguments
+						history -s "$action $context $arguments"
 
-		# interactive shell
-			while ($zen_alive && ! $zen_restart)
-			do
-				read -e -p "Zen> " action context arguments
-				history -s "$action $context $arguments"
+						if [[ $action = zen && -z $context ]]; then
+							echo "Can it be more zen than this?"
+							continue
+						else
+							zen $action $context $arguments
+						fi
+					done
 
-				if [[ $action != zen ]]; then
-					#{
-					#	# shell commands
-					#	echo "1"
-					#	eval "$action $context"
-					#} 2>/dev/null || 
-					{
-						# action based context
-						#echo "2"
-						eval "zen_$context $action $arguments"
-					} 2>/dev/null || {
-						# context based actions 
-						#echo "3"
-						eval "zen_$action $context $arguments"
-					} 2>/dev/null || {
-						# command from profile
-						#echo "4"
-						eval "zen_profile $action $context"
-					} 2>/dev/null || {
-						# shell commands
-						#echo "5"
-						eval "$action $context"
-					} 
-				else
-					echo "Can it be more zen than this?"
-				fi
-			done
+					if [[ $zen_restart ]]; then
+						zen_restart=false
+						zen
+						return
+					fi
+			;;
 
-			if [[ $zen_restart ]]; then
-				zen_restart=false
-				zen
-				return
-			fi
+			# interpret commands
+			*)
+				action=$1
+				context=$2
+				arguments=${@:2}
+
+				echo "action: $action"
+				echo "context: $context"
+				echo "arguments: $arguments"
+
+				{
+					# action from profile
+					eval "zen_profile $action $context"
+				} 2>/dev/null || {
+					# action based context
+					eval "zen_$context $action $arguments"
+				} 2>/dev/null || {
+					# context based actions 
+					eval "zen_$action $context $arguments"
+				} 2>/dev/null || {
+					# shell commands
+					eval "$action $context"
+				} 
+			;;
+
+		esac
 	}
 
